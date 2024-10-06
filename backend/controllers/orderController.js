4866aec64ad6a16ffca81dce01f54a3783a07565
Swapnil Ganvir <swapnilganvir55@gmail.com>
2024-10-06 14:40:42 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,7 +4,7 @@ import Stripe from 'stripe';
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = 'http://localhost:5173';
+  const frontend_url = "https://food-del-frontend-nztl.onrender.com";
 
   try {
     const newOrder = new orderModel({
