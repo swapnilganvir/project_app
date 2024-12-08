6817e5830e5702260027e1baab05280d2dc14ccb
Swapnil Ganvir <swapnilganvir55@gmail.com>
2024-12-08 14:39:31 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,8 @@ import { StoreContext } from '../../context/StoreContext';
 const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState('home');
 
-  const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
+  const { getTotalCartAmount, token, setToken, searchValue, setSearchValue } =
+    useContext(StoreContext);
 
   const navigate = useNavigate();
 
@@ -17,67 +18,97 @@ const Navbar = ({ setShowLogin }) => {
     navigate('/');
   }
 
+  function handleSearch(event) {
+    const { value } = event.target;
+    setSearchValue(value);
+  }
+
   return (
-    <div className="navbar">
-      <Link to="/">
-        <img src={assets.logo} alt="" className="logo" />
-      </Link>
-      <ul className="navbar-menu">
-        <Link
-          to="/"
-          onClick={() => setMenu('home')}
-          className={menu === 'home' ? 'active' : ''}
-        >
-          home
+    <div className="navbar-container">
+      <div className="navbar">
+        <Link to="/">
+          <img src={assets.logo} alt="" className="logo" />
         </Link>
-        <a
-          href="#explore-menu"
-          onClick={() => setMenu('menu')}
-          className={menu === 'menu' && 'active'}
-        >
-          menu
-        </a>
-        <a
-          href="#app-download"
-          onClick={() => setMenu('mobile-app')}
-          className={menu === 'mobile-app' && 'active'}
-        >
-          mobile-app
-        </a>
-        <a
-          href="#footer"
-          onClick={() => setMenu('contact-us')}
-          className={menu === 'contact-us' && 'active'}
-        >
-          contact us
-        </a>
-      </ul>
-      <div className="navbar-right">
-        <img src={assets.search_icon} alt="" />
-        <div className="navbar-search-icon">
-          <Link to="/cart">
-            <img src={assets.basket_icon} alt="" />
+        <ul className="navbar-menu">
+          <Link
+            to="/"
+            onClick={() => setMenu('home')}
+            className={menu === 'home' ? 'active' : ''}
+          >
+            home
           </Link>
-          <div className={getTotalCartAmount() && 'dot'}></div>
-        </div>
-        {!token ? (
-          <button onClick={() => setShowLogin(true)}>sign in</button>
-        ) : (
-          <div className="navbar-profile">
-            <img src={assets.profile_icon} alt="" />
-            <ul className="navbar-profile-dropdown">
-              <li onClick={() => navigate('/myorders')}>
-                <img src={assets.bag_icon} alt="" />
-                <p>Orders</p>
-              </li>
-              <hr />
-              <li onClick={logout}>
-                <img src={assets.logout_icon} alt="" />
-                <p>Logout</p>
-              </li>
-            </ul>
+          <a
+            href="#explore-menu"
+            onClick={() => setMenu('menu')}
+            className={menu === 'menu' && 'active'}
+          >
+            menu
+          </a>
+          <a
+            href="#app-download"
+            onClick={() => setMenu('mobile-app')}
+            className={menu === 'mobile-app' && 'active'}
+          >
+            mobile-app
+          </a>
+          <a
+            href="#footer"
+            onClick={() => setMenu('contact-us')}
+            className={menu === 'contact-us' && 'active'}
+          >
+            contact us
+          </a>
+        </ul>
+
+        <div className="navbar-right">
+          <div className="navbar-search-icon">
+            <img src={assets.search_icon} alt="" />
+            <input
+              name="search"
+              type="text"
+              placeholder="Search for a dish"
+              value={searchValue}
+              onChange={handleSearch}
+            />
           </div>
-        )}
+          <div className="navbar-basket-icon">
+            <Link to="/cart">
+              <img src={assets.basket_icon} alt="" />
+            </Link>
+            <div className={getTotalCartAmount() && 'dot'}></div>
+          </div>
+
+          {!token ? (
+            <button onClick={() => setShowLogin(true)}>sign in</button>
+          ) : (
+            <div className="navbar-profile">
+              <img src={assets.profile_icon} alt="" />
+              <ul className="navbar-profile-dropdown">
+                <li onClick={() => navigate('/myorders')}>
+                  <img src={assets.bag_icon} alt="" />
+                  <p>Orders</p>
+                </li>
+                <hr />
+                <li onClick={logout}>
+                  <img src={assets.logout_icon} alt="" />
+                  <p>Logout</p>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="searchbar">
+        <div className="navbar-search-icon">
+          <img src={assets.search_icon} alt="" />
+          <input
+            name="search"
+            type="text"
+            placeholder="Search for a dish"
+            value={searchValue}
+            onChange={handleSearch}
+          />
+        </div>
       </div>
     </div>
   );
