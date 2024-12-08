4175d1a351e188aec9360e540ddcc8e2b95d292d
Swapnil Ganvir <swapnilganvir55@gmail.com>
2024-12-08 14:37:53 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -4,26 +4,30 @@ import { StoreContext } from '../../context/StoreContext';
 import FoodItem from '../FoodItem/FoodItem';
 
 const FoodDisplay = ({ category }) => {
-  const { food_list } = useContext(StoreContext);
+  const { food_list, searchValue } = useContext(StoreContext);
 
   return (
     <div className="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item, index) => {
-          if (category === 'All' || category === item.category) {
-            return (
-              <FoodItem
-                key={index}
-                id={item._id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-                image={item.image}
-              />
-            );
-          }
-        })}
+        {food_list
+          .filter(item =>
+            item.name.toLowerCase().includes(searchValue.toLowerCase())
+          )
+          .map((item, index) => {
+            if (category === 'All' || category === item.category) {
+              return (
+                <FoodItem
+                  key={index}
+                  id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                  image={item.image}
+                />
+              );
+            }
+          })}
       </div>
     </div>
   );
